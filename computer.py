5406068cffe0774bcc57c63185d4c609faa88de5
Your Name <you@example.com>
2023-02-04 18:23:41 -0500

    added attributes, constructor, and methods

--- computer.py
+++ computer.py
@@ -2,10 +2,30 @@ class Computer:
 
     # What attributes will it need?
 
+    itemID: int = 0
+    yearMade: int = 0
+    ogPrice: bool = 0.0
+    operatingSytem: str = ""
+
+  
     # How will you set up your constructor?
-    
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    # idk what this means
+
+    def __init__(self, itemID, yearMade, ogPrice, operatingSystem):
+        self.itemID = itemID
+        self.yearMade = yearMade
+        self.ogPrice = ogPrice
+        self.operatingSystem = operatingSystem
 
     # What methods will you need?
+
+    def updateOS(self, operatingSystem):
+        if refurbish(): # refurbish method from oo_resale_shop.py
+            if operatingSystem not in "old OS" or "older OS" or "oldest OS":
+                operatingSystem = "the newest OS"
+            else:
+                print("Operating System already updated!")
+        
+    def deleteID(self, itemID):
+        if sellComputer(): # sell method from oo_resale_shop.py
+            del itemID
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,11 +1,59 @@
+from typing import Dict, Union, Optional
+
 class ResaleShop:
 
     # What attributes will it need?
 
+    inventory: dict = {}
+   
+
     # How will you set up your constructor?
-    
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    # idk what this wants me to do
+
+
+    def __init__(self, inventory):
+        self.inventory = inventory
+
 
     # What methods will you need?
+
+    def establishInventory(self, itemID, yearMade, ogPrice, operatingSystem):
+        self.inventory = {itemID: {yearMade, ogPrice, operatingSystem}}
+    
+    def buyComputer(self, itemID, yearMade, ogPrice, operatingSystem):
+        itemID +=1
+        self.inventory = self.inventory[itemID, {yearMade, ogPrice, operatingSystem}]
+
+    def updatePrice(self, itemID, newPrice):
+        if itemID in self.inventory:
+            self.inventory[itemID]["price"] = newPrice
+        else:
+            print("Item", itemID, "not found.")
+
+    def sellComputer(self, itemID):
+        if itemID in self.inventory:
+            del self.inventory[itemID]
+            print("Item", itemID, "sold!")
+        else: 
+            print("Item", itemID, "not found. Please select another item to sell.")
+
+    def printInventory(self, itemID):
+        if self.inventory:
+            for itemID in self.inventory:
+                print(f'Item ID: {itemID} : {self.inventory[itemID]}')
+        else:
+            print("No inventory to display.")
+
+    def refurbish(self, itemID):
+        if itemID in self.inventory:
+            computer = self.inventory[itemID]
+            if int(computer["yearMade"]) < 2000:
+                computer["newPrice"] = 0 
+            elif int(computer["yearMade"]) < 2012:
+                computer["newPrice"] = 250
+            elif int(computer["yearMade"]) < 2018:
+                computer["newPrice"] = 550
+            else:
+                computer["newPrice"] = 1000
+        else:
+            print("Item", itemID, "not found. Please select another item to refurbish.")
