c1d2abb22bfef7585002b4ef85fdcbc345998152
Your Name <you@example.com>
2023-02-05 15:17:25 -0500

    tried to run main

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,4 +1,4 @@
-from typing import Dict, Union, Optional
+# from typing import Dict, Union, Optional
 
 class ResaleShop:
 
@@ -17,43 +17,52 @@ class ResaleShop:
 
     # What methods will you need?
 
-    def establishInventory(self, itemID, yearMade, ogPrice, operatingSystem):
-        self.inventory = {itemID: {yearMade, ogPrice, operatingSystem}}
-    
-    def buyComputer(self, itemID, yearMade, ogPrice, operatingSystem):
-        itemID +=1
-        self.inventory = self.inventory[itemID, {yearMade, ogPrice, operatingSystem}]
+        def establishInventory(self, itemID, yearMade, ogPrice, operatingSystem):
+            self.inventory = {itemID: {yearMade, ogPrice, operatingSystem}}
+        
+        def buyComputer(self, itemID, yearMade, ogPrice, operatingSystem):
+            itemID +=1
+            self.inventory = self.inventory[itemID, {yearMade, ogPrice, operatingSystem}]
 
-    def updatePrice(self, itemID, newPrice):
-        if itemID in self.inventory:
-            self.inventory[itemID]["price"] = newPrice
-        else:
-            print("Item", itemID, "not found.")
-
-    def sellComputer(self, itemID):
-        if itemID in self.inventory:
-            del self.inventory[itemID]
-            print("Item", itemID, "sold!")
-        else: 
-            print("Item", itemID, "not found. Please select another item to sell.")
-
-    def printInventory(self, itemID):
-        if self.inventory:
-            for itemID in self.inventory:
-                print(f'Item ID: {itemID} : {self.inventory[itemID]}')
-        else:
-            print("No inventory to display.")
-
-    def refurbish(self, itemID):
-        if itemID in self.inventory:
-            computer = self.inventory[itemID]
-            if int(computer["yearMade"]) < 2000:
-                computer["newPrice"] = 0 
-            elif int(computer["yearMade"]) < 2012:
-                computer["newPrice"] = 250
-            elif int(computer["yearMade"]) < 2018:
-                computer["newPrice"] = 550
+        def updatePrice(self, itemID, newPrice):
+            if itemID in self.inventory:
+                self.inventory[itemID]["price"] = newPrice
             else:
-                computer["newPrice"] = 1000
-        else:
-            print("Item", itemID, "not found. Please select another item to refurbish.")
+                print("Item", itemID, "not found.")
+
+        def sellComputer(self, itemID):
+            if itemID in self.inventory:
+                del self.inventory[itemID]
+                print("Item", itemID, "sold!")
+            else: 
+                print("Item", itemID, "not found. Please select another item to sell.")
+
+        def printInventory(self, itemID):
+            if self.inventory:
+                for itemID in self.inventory:
+                    print(f'Item ID: {itemID} : {self.inventory[itemID]}')
+            else:
+                print("No inventory to display.")
+
+        def refurbish(self, itemID):
+            if itemID in self.inventory:
+                computer = self.inventory[itemID]
+                if int(computer["yearMade"]) < 2000:
+                    computer["newPrice"] = 0 
+                elif int(computer["yearMade"]) < 2012:
+                    computer["newPrice"] = 250
+                elif int(computer["yearMade"]) < 2018:
+                    computer["newPrice"] = 550
+                else:
+                    computer["newPrice"] = 1000
+            else:
+                print("Item", itemID, "not found. Please select another item to refurbish.")
+
+
+        
+    def main():
+
+        computerOne = ResaleShop("01", {"2002, 3000, older OS"})
+        computerOne.print()
+    
+    main()
